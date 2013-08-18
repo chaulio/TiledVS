1af8e8f12dd121021235959f7503f1fc12cba3bc
jaquelline <alva.jaque@gmail.com>
2013-08-18 15:53:09 -0300

    Update tiledMatrix.cpp

--- tiledMatrix.cpp
+++ tiledMatrix.cpp
@@ -17,7 +17,7 @@
  */
  
  /**
- * TiledVS - TiledMatrix was designed to support the development
+ * TiledMatrix - TiledMatrix was designed to support the development
  * of applications that require the manipulation of huge matrices in 
  * external memory. And, as is known, any application requiring external 
  * memory processing should be designed focusing the I/O operations reduction.
