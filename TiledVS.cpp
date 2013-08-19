816d37e4993b0549cba9dcdeaac937b62c9e0743
chaulio <chaulio@hotmail.com>
2013-08-19 08:17:45 -0300

    Update TiledVS.cpp

--- TiledVS.cpp
+++ TiledVS.cpp
@@ -1,19 +1,28 @@
-/** TiledVS 1.0. Date of release: July 25th, 2013. 
- *
+/**
+ * Copyright (c) 2013, Chaulio R. Ferreira, Marcus V.A. Andrade, Salles V.G. Magalhaes, 
+ * W. Randolph Franklin and Andre M. Pompermayer.
+ * All rights reserved.
  * 
- * Copyright (C) 2013 Chaulio Ferreira, Marcus Andrade, Salles Magalhaes,
- * W. Randolph Franklin and Andre Pompermayer.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
  * 
- * This program is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by the 
- * Free Software Foundation, either version 3 of the License, or (at your 
- * option) any later version.
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
- * for more details.
- * You should have received a copy of the GNU General Public License along 
- * with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ *   Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
 /**
@@ -23,12 +32,12 @@
  * 
  * For further information, consider the following reference:
  * 
- * [1] Ferreira, C.R., et al., 2012. More efficient terrain viewshed 
+ * Ferreira, C.R., et al., 2012. More efficient terrain viewshed 
  * computation on massive datasets using external memory. In: Proceedings
  * of the 20th International Conference on Advances in Geographic 
  * Information Systems, SIGSPATIAL ’12, Redondo Beach, California, 
  * USA: ACM, 494–497.
- * 
+ * Available at: http://www.ecse.rpi.edu/~wrf/p/159-acmgis2012-viewshed.pdf
  */
 
 
